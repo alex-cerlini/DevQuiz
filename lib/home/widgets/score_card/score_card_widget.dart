704a65488a251ca8d4934a5b67d1446754ed0d90
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-19 15:39:02 -0300

    New widget: ScoreCard in top page

---
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+// import 'package:dev_quiz/core/core.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
