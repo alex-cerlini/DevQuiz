1b7633baef3bb6cbd3aa49d1c5214dcd031d7e3c
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-19 15:33:12 -0300

    Applying style on appbar

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -9,21 +9,36 @@ class AppBarWidget extends PreferredSize {
           child: Container(
             height: 250,
             decoration: BoxDecoration(gradient: AppGradients.linear),
-            child: Row(
-              children: [
-                Text("Olá, Alex"),
-                Container(
-                  width: 58,
-                  height: 58,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(
-                      image: NetworkImage(
-                          "https://avatars.githubusercontent.com/u/56663683?v=4"),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text.rich(
+                    TextSpan(
+                      text: "Olá, ",
+                      style: AppTextStyles.title,
+                      children: [
+                        TextSpan(
+                          text: "Alex",
+                          style: AppTextStyles.titleBold,
+                        ),
+                      ],
                     ),
                   ),
-                ),
-              ],
+                  Container(
+                    width: 58,
+                    height: 58,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            "https://avatars.githubusercontent.com/u/56663683?v=4"),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         );
