8c115b0d1106b181ca13253ce1163d713de15f8f
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-19 16:15:59 -0300

    Finished style of score card on appbar

--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -1,29 +1,43 @@
 import 'package:dev_quiz/home/widgets/chart/chart_widget.dart';
 import 'package:flutter/material.dart';
-// import 'package:dev_quiz/core/core.dart';
+import 'package:dev_quiz/core/core.dart';
 
 class ScoreCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          flex: 1,
-          child: ChartWidget(),
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Container(
+        height: 136,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(15),
         ),
-        Expanded(
-          flex: 3,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 24),
-            child: Column(
-              children: [
-                Text("Vamos começar"),
-                Text("Complete os desafios e avance em conhecimento"),
-              ],
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: ChartWidget(),
             ),
-          ),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Vamos começar", style: AppTextStyles.heading),
+                    Text("Complete os desafios e avance em conhecimento",
+                        style: AppTextStyles.body),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
