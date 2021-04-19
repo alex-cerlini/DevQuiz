efc8b20f420db1509705486f58d40980be8d5441
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-19 15:26:41 -0300

    Adding welcome user on appbar

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -7,6 +7,24 @@ class AppBarWidget extends PreferredSize {
       : super(
           preferredSize: Size.fromHeight(250),
           child: Container(
-              decoration: BoxDecoration(gradient: AppGradients.linear)),
+            height: 250,
+            decoration: BoxDecoration(gradient: AppGradients.linear),
+            child: Row(
+              children: [
+                Text("Olá, Alex"),
+                Container(
+                  width: 58,
+                  height: 58,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          "https://avatars.githubusercontent.com/u/56663683?v=4"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         );
 }
