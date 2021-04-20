70e1ab6d96e60245e4c514cf8479b16c4dd3e3d2
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-20 20:47:15 -0300

    Import core and add title var

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,7 +1,9 @@
+import 'package:dev_quiz/core/core.dart';
 import 'package:flutter/material.dart';
 
 class QuizWidget extends StatelessWidget {
-  const QuizWidget({Key? key}) : super(key: key);
+  final String title;
+  const QuizWidget({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
