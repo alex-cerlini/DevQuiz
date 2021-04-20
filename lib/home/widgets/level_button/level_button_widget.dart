751c09a6c02598ddd58bb8bb1afc73e5bb8362fc
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-20 12:34:12 -0300

    Adding decoration color and import core.dart

--- lib/home/widgets/level_button/level_button_widget.dart
+++ lib/home/widgets/level_button/level_button_widget.dart
@@ -1,9 +1,14 @@
+import 'package:dev_quiz/core/core.dart';
 import 'package:flutter/material.dart';
 
 class LevelButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        color: AppColors.levelButtonFacil,
+        borderRadius: BorderRadius.circular(28),
+      ),
       child: Text("Fácil"),
     );
   }
