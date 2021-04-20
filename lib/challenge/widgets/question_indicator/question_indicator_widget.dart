1140d99a099451300cb760f4dd939e2a7ddd6b5d
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-20 19:24:48 -0300

    Key now is optional

--- lib/challenge/widgets/question_indicator/question_indicator_widget.dart
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class QuestionIndicatorWidget extends StatelessWidget {
-  const QuestionIndicatorWidget({Key key}) : super(key: key);
+  const QuestionIndicatorWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
