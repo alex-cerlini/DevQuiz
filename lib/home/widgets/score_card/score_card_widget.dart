d96622fa7042755a460e1f05e180b8ad2c4b2da4
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-20 12:23:55 -0300

    Right value for padding row

--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -14,7 +14,7 @@ class ScoreCardWidget extends StatelessWidget {
           borderRadius: BorderRadius.circular(15),
         ),
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.symmetric(horizontal: 24),
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
