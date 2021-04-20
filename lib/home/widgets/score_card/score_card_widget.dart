4a6c4d21ce23412f388924bee157dbb3eac917a2
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-20 12:19:48 -0300

    Main Alignment on center

--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -14,6 +14,7 @@ class ScoreCardWidget extends StatelessWidget {
           borderRadius: BorderRadius.circular(15),
         ),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Expanded(
