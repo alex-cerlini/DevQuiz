445d71a91cb3ec258dfe7f4c582f3dfdb2b4f01a
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-20 19:08:54 -0300

    New folder and .dart file: challenge_page

---
+++ lib/challenge/challenge_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Challengepage extends StatefulWidget {
+  Challengepage({Key? key}) : super(key: key);
+
+  @override
+  _ChallengepageState createState() => _ChallengepageState();
+}
+
+class _ChallengepageState extends State<Challengepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
