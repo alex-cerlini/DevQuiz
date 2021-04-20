43d283e095e6d09c10f3b53b27d357f3ed9f380e
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-20 20:07:48 -0300

    Reppading question indicator

--- lib/challenge/widgets/question_indicator/question_indicator_widget.dart
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -8,22 +8,22 @@ class QuestionIndicatorWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-            ),
-            child: Row(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
+        child: Column(
+          children: [
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text("Questão 04", style: AppTextStyles.body),
                 Text("de 10", style: AppTextStyles.body),
               ],
             ),
-          ),
-          ProgressIndicatorWidget(value: 0.4),
-        ],
+            ProgressIndicatorWidget(value: 0.4),
+          ],
+        ),
       ),
     );
   }
