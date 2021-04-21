72ecc008e0fb8784c2cd6ea8b27a2b846db469b7
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-21 18:23:41 -0300

    Importing HomeState and creating a var with him

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,7 +1,10 @@
+import 'package:dev_quiz/home/home_state.dart';
 import 'package:dev_quiz/shared/widgets/progress_indicator/models/quiz_model.dart';
 import 'package:dev_quiz/shared/widgets/progress_indicator/models/user_model.dart';
 
 class HomeController {
+  HomeState state = HomeState.empty;
+
   UserModel? user;
   List<QuizModel>? quizzes;
 }
