4bda169e8ba3e8ca26d222bd0ac38620c5d7cf58
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-21 18:37:05 -0300

    Missing new import

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,4 +1,5 @@
 import 'package:dev_quiz/home/home_state.dart';
+import 'package:dev_quiz/shared/widgets/progress_indicator/models/awnser_model.dart';
 import 'package:dev_quiz/shared/widgets/progress_indicator/models/question_model.dart';
 import 'package:dev_quiz/shared/widgets/progress_indicator/models/quiz_model.dart';
 import 'package:dev_quiz/shared/widgets/progress_indicator/models/user_model.dart';
