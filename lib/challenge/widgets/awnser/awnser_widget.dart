3ce3aeba40feac3b16c0576377697b35a35d54d0
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-20 22:36:40 -0300

    Padding and comment a var for run app

--- lib/challenge/widgets/awnser/awnser_widget.dart
+++ lib/challenge/widgets/awnser/awnser_widget.dart
@@ -3,40 +3,45 @@ import 'package:flutter/material.dart';
 
 class AwnserWidget extends StatelessWidget {
   final String title;
-  final bool isRight;
-  final bool isSelected;
+  // final bool isRight;
+  // final bool isSelected;
 
   const AwnserWidget({
     Key? key,
     required this.title,
-    required this.isRight,
-    required this.isSelected,
+    // required this.isRight,
+    // required this.isSelected,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: AppColors.white,
-        borderRadius: BorderRadius.circular(10),
-        border: Border.fromBorderSide(
-          BorderSide(color: AppColors.border),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.fromBorderSide(
+            BorderSide(color: AppColors.border),
+          ),
         ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            title,
-            style: AppTextStyles.body,
-          ),
-          Container(
-            width: 24,
-            height: 24,
-            child: Icon(Icons.check),
-          ),
-        ],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Text(
+                title,
+                style: AppTextStyles.body,
+              ),
+            ),
+            Container(
+              width: 24,
+              height: 24,
+              child: Icon(Icons.check),
+            ),
+          ],
+        ),
       ),
     );
   }
