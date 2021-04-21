9193f10a8880411a085aea21baf27210dcacc7ff
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-21 18:38:55 -0300

    Import HomeController to Home_page

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:dev_quiz/home/home_controller.dart';
 import 'package:dev_quiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 import './widgets/appbar/app_bar_widget.dart';
@@ -9,6 +10,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
