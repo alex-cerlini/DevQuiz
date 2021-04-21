06c5d9eb3dda18a6d0e35d4285c2ca64e70de1d1
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-21 18:20:21 -0300

    Creating class HomeController with 2 vars

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,2 +1,7 @@
 import 'package:dev_quiz/shared/widgets/progress_indicator/models/quiz_model.dart';
 import 'package:dev_quiz/shared/widgets/progress_indicator/models/user_model.dart';
+
+class HomeController {
+  UserModel? user;
+  List<QuizModel>? quizzes;
+}
