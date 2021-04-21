67ca23f4e159d8d24444f6febb9e84f23127e31e
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-21 17:20:41 -0300

    Declarating new vars

--- lib/shared/widgets/progress_indicator/models/quiz_model.dart
+++ lib/shared/widgets/progress_indicator/models/quiz_model.dart
@@ -3,4 +3,12 @@ import 'package:dev_quiz/shared/widgets/progress_indicator/models/question_model
 class QuizModel {
   final String title;
   final List<QuestionModel> questions;
+  final int questionAwnsered;
+  final String imagem;
+  final Level level;
+
+  QuizModel({
+    required this.title,
+    required this.questions,
+  });
 }
