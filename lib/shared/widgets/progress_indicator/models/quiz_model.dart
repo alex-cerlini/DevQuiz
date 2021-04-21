1c23c87719267d3773aba56020380596c4ada527
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-21 17:17:06 -0300

    Import QuestionModel to quiz_model

--- lib/shared/widgets/progress_indicator/models/quiz_model.dart
+++ lib/shared/widgets/progress_indicator/models/quiz_model.dart
@@ -1,5 +1,6 @@
+import 'package:dev_quiz/shared/widgets/progress_indicator/models/question_model.dart';
+
 class QuizModel {
   final String title;
-  final List<> questions;
+  final List<QuestionModel> questions;
 }
-
