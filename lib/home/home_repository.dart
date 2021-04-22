bdb8286560425f98b58abc25e1b07dc4dae91e53
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-22 12:40:21 -0300

    two futures asyncs added empty

--- lib/home/home_repository.dart
+++ lib/home/home_repository.dart
@@ -1,1 +1,7 @@
+import 'package:dev_quiz/shared/widgets/progress_indicator/models/quiz_model.dart';
+import 'package:dev_quiz/shared/widgets/progress_indicator/models/user_model.dart';
 
+class HomeRepository {
+  Future<UserModel> getUser() async {}
+  Future<List<QuizModel>> getQuizzes() async {}
+}
