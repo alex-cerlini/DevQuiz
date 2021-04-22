7b3a6b5ebe131f5ab72d20bb5ee521bd3767d235
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-22 12:54:00 -0300

    finished getUser

--- lib/home/home_repository.dart
+++ lib/home/home_repository.dart
@@ -1,7 +1,13 @@
 import 'package:dev_quiz/shared/widgets/progress_indicator/models/quiz_model.dart';
 import 'package:dev_quiz/shared/widgets/progress_indicator/models/user_model.dart';
+import 'package:flutter/services.dart';
 
 class HomeRepository {
-  Future<UserModel> getUser() async {}
+  Future<UserModel> getUser() async {
+    final response = await rootBundle.loadString("/database/user.json");
+    final user = UserModel.fromJson(response);
+    return user;
+  }
+
   Future<List<QuizModel>> getQuizzes() async {}
 }
