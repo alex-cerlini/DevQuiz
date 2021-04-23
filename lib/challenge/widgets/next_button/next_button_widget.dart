0daf3591ef652ecc2fedc58fb941939022c09eeb
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-23 18:44:34 -0300

    Basic of NextButton created

--- lib/challenge/widgets/next_button/next_button_widget.dart
+++ lib/challenge/widgets/next_button/next_button_widget.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class NextButtonWidget extends StatelessWidget {
+  final String label;
+  NextButtonWidget({required this.label})
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextButton(onPressed: () {}, child: Text(label));
   }
 }
