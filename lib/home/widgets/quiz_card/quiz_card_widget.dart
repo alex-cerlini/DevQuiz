606002c25676a72fc9cd815ad8598476841fea9c
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-24 16:23:00 -0300

    Set a GestureDetector with onTap

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -18,48 +18,51 @@ class QuizCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        border: Border.fromBorderSide(
-          BorderSide(color: AppColors.border),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border.fromBorderSide(
+            BorderSide(color: AppColors.border),
+          ),
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(10),
         ),
-        color: AppColors.white,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 40,
-            width: 40,
-            child: Image.asset(AppImages.data),
-          ),
-          SizedBox(
-            height: 21,
-          ),
-          Text(title, style: AppTextStyles.heading15),
-          SizedBox(
-            height: 21,
-          ),
-          Row(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Text(
-                  completed,
-                  style: AppTextStyles.body11,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 40,
+              width: 40,
+              child: Image.asset(AppImages.data),
+            ),
+            SizedBox(
+              height: 21,
+            ),
+            Text(title, style: AppTextStyles.heading15),
+            SizedBox(
+              height: 21,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Text(
+                    completed,
+                    style: AppTextStyles.body11,
+                  ),
                 ),
-              ),
-              Expanded(
-                flex: 3,
-                child: ProgressIndicatorWidget(
-                  value: percent,
+                Expanded(
+                  flex: 3,
+                  child: ProgressIndicatorWidget(
+                    value: percent,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
