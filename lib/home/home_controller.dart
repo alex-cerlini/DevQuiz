6e8b64e4d76fccf163fb1f2b40f78e46761cf4b6
Alexander Andrade Cerlini <alexcerlinii@gmail.com>
2021-04-24 11:48:48 -0300

    Removing unused import

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,8 +1,5 @@
-import 'package:dev_quiz/core/core.dart';
 import 'package:dev_quiz/home/home_repository.dart';
 import 'package:dev_quiz/home/home_state.dart';
-import 'package:dev_quiz/shared/widgets/progress_indicator/models/awnser_model.dart';
-import 'package:dev_quiz/shared/widgets/progress_indicator/models/question_model.dart';
 import 'package:dev_quiz/shared/widgets/progress_indicator/models/quiz_model.dart';
 import 'package:dev_quiz/shared/widgets/progress_indicator/models/user_model.dart';
 import 'package:flutter/foundation.dart';
